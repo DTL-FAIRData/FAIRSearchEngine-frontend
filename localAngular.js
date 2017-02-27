1994c2dac6e81d9f2413beb51b9778e8fb318720
nunogit <nunosn@iol.pt>
2017-02-27 13:15:02 +0100

    documentation update

---
+++ localAngular.js
@@ -0,0 +1,6 @@
+//var app = angular.module('previewSearchResult', []);
+
+//app.controller('previewSearchResult', function($scope) {
+//    $scope.firstName = "John";
+//    $scope.lastName = "Doe";
+//});
