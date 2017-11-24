72b697f8e4e05b440014cd8696e2911d0da62b1d
nunogit <nunosn@iol.pt>
2017-11-24 23:34:03 +0100

    interface fixes

---
+++ elastic-client.js
@@ -0,0 +1,7 @@
+
+// place holder
+function getFairDataPointList(){
+    fdpList = Array();
+    fdpList[0] = "http://dev-vm.fair-dtls.surf-hosted.nl:8082/fdp/"
+    fdpList[1] = "http://dev-vm.fair-dtls.surf-hosted.nl:8082/fdp/";
+}
